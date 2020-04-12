bed99b1602e007e3c4901cc06df24336d776ca0d
Thomas Metcalfe <tommetcalfe@live.com>
2020-04-12 10:41:17 +0200

    Separate key retrieval method, add test case

--- twitter-on-slack/main.py
+++ twitter-on-slack/main.py
@@ -1,25 +1,28 @@
 from twitter import Api, Status
 import os
+from typing import List
 
 
-def pull_and_post(consumer_key: str,
-                  consumer_secret: str,
-                  access_token: str,
-                  access_token_secret: str):
+def pull_and_post(
+    consumer_key: str, consumer_secret: str, access_token: str, access_token_secret: str
+):
 
-    twitter_api = Api(consumer_key,
-                      consumer_secret,
-                      access_token,
-                      access_token_secret,
-                      application_only_auth=True)
+    twitter_api = Api(
+        consumer_key,
+        consumer_secret,
+        access_token,
+        access_token_secret,
+        application_only_auth=True,
+    )
 
 
-def main():
+def _retrieve_keys() -> List[str]:
     env_vars = (
-    "TWITTER_CONSUMER_KEY",
-    "TWITTER_CONSUMER_SECRET",
-    "TWITTER_ACCESS_TOKEN",
-    "TWITTER_ACCESS_TOKEN_SECRET")
+        "TWITTER_CONSUMER_KEY",
+        "TWITTER_CONSUMER_SECRET",
+        "TWITTER_ACCESS_TOKEN",
+        "TWITTER_ACCESS_TOKEN_SECRET",
+    )
 
     keys = []
 
@@ -29,5 +32,9 @@ def main():
             raise IOError(f"Missing required env var: {env_var}")
 
         keys.append(value)
+    return keys
 
+
+def main():
+    keys = _retrieve_keys()
     pull_and_post(*keys)
