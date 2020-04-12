d4e27774c8d51a081683f95743544f0f728b31df
Thomas Metcalfe <tommetcalfe@live.com>
2020-04-12 10:52:48 +0200

    Set wait time, slack channel and make while loop

--- twitter-on-slack/main.py
+++ twitter-on-slack/main.py
@@ -1,10 +1,11 @@
 from twitter import Api, Status
+from slack import WebClient
 import os
-from typing import List
+from typing import List, Dict, Any
 
 
 def pull_and_post(
-    consumer_key: str, consumer_secret: str, access_token: str, access_token_secret: str
+    consumer_key: str, consumer_secret: str, access_token: str, access_token_secret: str, slack_token: str, slack_channel: str, wait_time: int = 60,
 ):
 
     twitter_api = Api(
@@ -15,6 +16,17 @@ def pull_and_post(
         application_only_auth=True,
     )
 
+    slack_client = WebClient(slack_token)
+    since_id = None
+
+    while True:
+        statuses = twitter_api.GetHomeTimeline(since_id=since_id)
+
+        for status in statuses:
+            slack_client.chat_postMessage(text=status.text, channel=slack_channel)
+
+            since_id = status.id
+
 
 def _retrieve_keys() -> List[str]:
     env_vars = (
@@ -22,6 +34,7 @@ def _retrieve_keys() -> List[str]:
         "TWITTER_CONSUMER_SECRET",
         "TWITTER_ACCESS_TOKEN",
         "TWITTER_ACCESS_TOKEN_SECRET",
+        "SLACK_API_TOKEN"
     )
 
     keys = []
@@ -35,6 +48,6 @@ def _retrieve_keys() -> List[str]:
     return keys
 
 
-def main():
+def main(slack_channel: str, wait_time: int):
     keys = _retrieve_keys()
-    pull_and_post(*keys)
+    pull_and_post(*keys, slack_channel=slack_channel, wait_time=wait_time)
