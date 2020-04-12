769b3c6add85bbb9ae01776c31295eabaf7a154f
Thomas Metcalfe <tommetcalfe@live.com>
2020-04-12 10:28:57 +0200

    Set up main function

--- twitter-on-slack/main.py
+++ twitter-on-slack/main.py
@@ -1,7 +1,11 @@
 from twitter import Api, Status
+import os
 
 
-def main(consumer_key: str, consumer_secret: str, access_token: str, access_token_secret: str):
+def pull_and_post(consumer_key: str,
+                  consumer_secret: str,
+                  access_token: str,
+                  access_token_secret: str):
 
     twitter_api = Api(consumer_key,
                       consumer_secret,
@@ -10,3 +14,20 @@ def main(consumer_key: str, consumer_secret: str, access_token: str, access_toke
                       application_only_auth=True)
 
 
+def main():
+    env_vars = (
+    "TWITTER_CONSUMER_KEY",
+    "TWITTER_CONSUMER_SECRET",
+    "TWITTER_ACCESS_TOKEN",
+    "TWITTER_ACCESS_TOKEN_SECRET")
+
+    keys = []
+
+    for env_var in env_vars:
+        value = os.environ.get(env_var)
+        if value is None:
+            raise IOError(f"Missing required env var: {env_var}")
+
+        keys.append(value)
+
+    pull_and_post(*keys)
